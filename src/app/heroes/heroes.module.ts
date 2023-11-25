1d99158d5edd1bdb7cd333cf926fc2851b9aa211
Joselu <joseluissgz97@gmail.com>
2023-11-25 18:35:07 +0100

    importing ReactiveFormsModule

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
 import { HeroCardComponent } from './components/hero-card/hero-card.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
@@ -10,7 +12,6 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { heroImagePipe } from './pipes/hero-image.pipe';
-import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
@@ -20,10 +21,16 @@ import { SpinnerComponent } from './components/spinner/spinner.component';
     NewPageComponent,
     SearchPageComponent,
     HeroCardComponent,
+
     //PIPES
     heroImagePipe,
     SpinnerComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
 })
 export class HeroesModule {}
