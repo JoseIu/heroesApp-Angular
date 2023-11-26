fc5aebb2b5814e9b212a400f10bcb483fd298e12
Joselu <joseluissgz97@gmail.com>
2023-11-26 11:49:37 +0100

    publuhers for select

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styleUrls: ['./new-page.component.scss']
+  styleUrls: ['./new-page.component.scss'],
 })
 export class NewPageComponent {
-
+  public publuhers = [
+    {
+      id: 'Marvel Comics',
+      text: 'Marvel-Comics',
+    },
+    {
+      id: 'DC Comics',
+      text: 'DC-Comics',
+    },
+  ];
 }
